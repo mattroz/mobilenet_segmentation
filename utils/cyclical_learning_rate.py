538a2aa5ac9d3bb36aa135e7fb580b0dc6832e7f
mattroz <matveyrozanov@gmail.com>
2019-04-22 17:48:46 +0300

    Implement optimal bounds search after warm-up CLR training

--- utils/cyclical_learning_rate.py
+++ utils/cyclical_learning_rate.py
@@ -22,11 +22,15 @@ class CyclicalLearningRateScheduler(keras.callbacks.History):
     def on_train_begin(self, logs=None):
         K.set_value(self.model.optimizer.lr, self.base_lr)
         if self.search_optimal_bounds:
-            n_epochs_for_search = 4
+            # Save model initial weights for further reinitialization
+            self.initial_weights = self.model.get_weights()
+            # Create array of learning rates to iterate over
+            self.n_epochs_for_search = self.step_size // 2
+            logs['epochs'] += self.n_epochs_for_search
             iters_in_epoch = self.params['steps']
             self.search_lrs = np.linspace(self.base_lr,
                                           self.max_lr,
-                                          n_epochs_for_search * iters_in_epoch)
+                                          self.n_epochs_for_search * iters_in_epoch)
             self.search_iteration = 1
             self.losses = np.array([])
 
@@ -42,15 +46,30 @@ class CyclicalLearningRateScheduler(keras.callbacks.History):
             updated_lr = self.base_lr + (self.max_lr - self.base_lr) * max(0, 1 - x)
         K.set_value(self.model.optimizer.lr, updated_lr)
 
-    def on_epoch_end(self, epoch, logs=None):
-        pass
+    def __calculate_optimal_bounds(self):
+        self.search_lrs = np.reshape(self.search_lrs, (-1, 1))
+        self.losses = np.reshape(self.losses, (-1, 1))
+        averaged_losses = np.array(self.losses[0])
 
-    def on_train_end(self, logs=None):
+        # Calculate moving average for optimal bounds searching
+        smooth = 0.05
+        for i in range(1, self.losses.shape[0]):
+            loss =  smooth * self.losses[i] + (1 - smooth) * averaged_losses[-1]
+            averaged_losses = np.append(averaged_losses, loss)
+
+        # Get learning rate with the highest loss and the lowest one
+        lowest_loss_idx = np.argmin(averaged_losses)
+        highest_loss_idx = np.argmax(averaged_losses)
+
+        return self.search_lrs[highest_loss_idx], self.search_lrs[lowest_loss_idx]
+
+    def on_epoch_end(self, epoch, logs=None):
         if self.search_optimal_bounds:
-            self.search_lrs = np.reshape(self.search_lrs, (-1,1))
-            self.losses = np.reshape(self.losses, (-1,1))
-            print(self.search_lrs.shape, self.losses.shape)
-            np.save('./losses.npy', self.losses)
-            np.save('./lrs.npy', self.search_lrs)
-            lrs_and_losses = np.concatenate((self.search_lrs, self.losses), axis=1)
-            np.save('./lrs_and_losses.npy', lrs_and_losses)
+            if epoch == self.n_epochs_for_search:
+                print("Stop searching for optimal bounds, calculating...")
+                self.base_lr, self.max_lr = self.__calculate_optimal_bounds()
+                print(f"Set base_lr: {self.base_lr}, max_lr: {self.max_lr}")
+                self.search_optimal_bounds = False
+                print(f"Reinitializing model...")
+                self.model.set_weights(self.initial_weights)
+                del self.initial_weights
