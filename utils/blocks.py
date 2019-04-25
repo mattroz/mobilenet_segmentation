6896a66df7d149f96ccac7f7d5448e3ca5a75b17
mattroz <matveyrozanov@gmail.com>
2019-04-25 12:51:24 +0300

    Add squeeze-and-excitation block

---
+++ utils/blocks.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+import keras
+import keras.backend as K
+import tensorflow as tf
+
+from keras.layers import Conv2D, GlobalAveragePooling2D, Multiply
+
+
+def SE_block(input_tensor, block_id, reduction_rate=16):
+    """Channel Squeeze and Excitation block
+    Paper: https://arxiv.org/pdf/1709.01507v1.pdf
+    """
+
+    tensor_shape = K.int_shape(input_tensor)
+    filters = tensor_shape[-1] if K.image_data_format() == 'channels_last' \
+        else tensor_shape[1]
+
+    over_reduction = filters // reduction_rate == 0
+    reduced_filters = (filters // reduction_rate) if not over_reduction else 1
+
+    squeeze_layer = GlobalAveragePooling2D(data_format=K.image_data_format(), name=f'squeeze_{block_id}')(input_tensor)
+    excitation_1 = Conv2D(kernel_size=(1, 1),
+                          filters=reduced_filters,
+                          activation='relu',
+                          name=f'excitate_1_{block_id}')(squeeze_layer)
+    excitation_2 = Conv2D(kernel_size=(1, 1),
+                          filters=filters,
+                          activation='sigmoid',
+                          name=f'excitate_2_{block_id}')(excitation_1)
+
+    return Multiply()([input_tensor, excitation_2], name=f'scale_{block_id}')
+
